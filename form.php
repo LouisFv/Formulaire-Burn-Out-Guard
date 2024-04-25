c77b74f8bd7b468898adf36be17dd262bb4686bb
Timothée <timothee.caron@ecoles-epsi.net>
2024-04-25 13:11:13 +0200

    v2

--- form.php
+++ form.php
@@ -10,7 +10,6 @@
     <nav class="navbar"><img src="images/logodetoure.png" alt="Logo" width="170px"></nav>
     <div class="Tittle_page" style="color: #001858;"><h1>Formulaire Burn Out Guard</h1></div>
         <div class="container">
-            <form action="" method="post">
                 <div class="form">
                     <label for="Information">Vos informations:</label>
                     <ul>
@@ -19,6 +18,7 @@
                         <li>Filière associé</li>
                     </ul>
 
+                <form action="" method="post">
                 <div class="form-group">
                     <label for="charge_travail">1. Comment vous sentez-vous par rapport à votre charge de travail actuelle ?</label><br>
                     <input type="radio" id="charge_travail_bien" name="charge_travail" value="Bien"> Bien
@@ -27,7 +27,9 @@
                     <input type="radio" id="charge_travail_plutot_mal" name="charge_travail" value="Plutôt mal"> Plutôt mal
                     <input type="radio" id="charge_travail_mal" name="charge_travail" value="Mal"> Mal
                 </div>
+                </form>
 
+                <form action="" method="post">
                 <div class="form-group">
                     <label for="charge_travail">2. Avez-vous l'impression d'être constamment sous pression ou débordé par vos responsabilités ?</label><br>
                     <input type="radio" id="charge_travail_bien" name="charge_travail" value="Bien"> Bien
@@ -36,7 +38,9 @@
                     <input type="radio" id="charge_travail_plutot_mal" name="charge_travail" value="Plutôt mal"> Plutôt mal
                     <input type="radio" id="charge_travail_mal" name="charge_travail" value="Mal"> Mal
                 </div>
+                </form>
 
+                <form action="" method="post">
                 <div class="form-group">
                     <label for="charge_travail">3. Êtes-vous satisfait de votre équilibre entre vie professionnelle et vie personnelle ?</label><br>
                     <input type="radio" id="charge_travail_bien" name="charge_travail" value="Bien"> Bien
@@ -45,7 +49,9 @@
                     <input type="radio" id="charge_travail_plutot_mal" name="charge_travail" value="Plutôt mal"> Plutôt mal
                     <input type="radio" id="charge_travail_mal" name="charge_travail" value="Mal"> Mal
                 </div>
+                </form>
 
+                <form action="" method="post">
                 <div class="form-group">
                     <label for="charge_travail">4. Avez-vous les ressources nécessaires pour accomplir vos tâches de manière efficace ?</label><br>
                     <input type="radio" id="charge_travail_bien" name="charge_travail" value="Bien"> Bien
@@ -54,7 +60,9 @@
                     <input type="radio" id="charge_travail_plutot_mal" name="charge_travail" value="Plutôt mal"> Plutôt mal
                     <input type="radio" id="charge_travail_mal" name="charge_travail" value="Mal"> Mal
                 </div>
+                </form>
 
+                <form action="" method="post">
                 <div class="form-group">
                     <label for="charge_travail">5. Avez-vous des difficultés à déconnecter du travail une fois votre journée terminée ?</label><br>
                     <input type="radio" id="charge_travail_bien" name="charge_travail" value="Bien"> Bien
@@ -63,7 +71,9 @@
                     <input type="radio" id="charge_travail_plutot_mal" name="charge_travail" value="Plutôt mal"> Plutôt mal
                     <input type="radio" id="charge_travail_mal" name="charge_travail" value="Mal"> Mal
                 </div>
+                </form>
 
+                <form action="" method="post">
                 <div class="form-group">
                     <label for="charge_travail">6. Quelles sont vos principales sources de stress au travail ?</label><br>
                     <input type="radio" id="charge_travail_bien" name="charge_travail" value="Bien"> Bien
@@ -72,7 +82,9 @@
                     <input type="radio" id="charge_travail_plutot_mal" name="charge_travail" value="Plutôt mal"> Plutôt mal
                     <input type="radio" id="charge_travail_mal" name="charge_travail" value="Mal"> Mal
                 </div>
+                </form>
 
+                <form action="" method="post">
                 <div class="form-group">
                     <label for="charge_travail">7. Y a-t-il des tâches spécifiques qui vous semblent particulièrement épuisantes mentalement ou émotionnellement ?  </label><br>
                     <input type="radio" id="charge_travail_bien" name="charge_travail" value="Bien"> Bien
@@ -81,7 +93,9 @@
                     <input type="radio" id="charge_travail_plutot_mal" name="charge_travail" value="Plutôt mal"> Plutôt mal
                     <input type="radio" id="charge_travail_mal" name="charge_travail" value="Mal"> Mal
                 </div>
+                </form>
 
+                <form action="" method="post">
                 <div class="form-group">
                     <label for="charge_travail">8. Avez-vous le sentiment d'avoir suffisamment de soutien de la part de vos collègues et de votre supérieur hiérarchique ?</label><br>
                     <input type="radio" id="charge_travail_bien" name="charge_travail" value="Bien"> Bien
@@ -90,7 +104,9 @@
                     <input type="radio" id="charge_travail_plutot_mal" name="charge_travail" value="Plutôt mal"> Plutôt mal
                     <input type="radio" id="charge_travail_mal" name="charge_travail" value="Mal"> Mal
                 </div>
+                </form>
 
+                <form action="" method="post">
                 <div class="form-group">
                     <label for="charge_travail">9. Comment percevez-vous votre niveau d'énergie et de motivation par rapport à il y a quelques semaines/mois ?</label><br>
                     <input type="radio" id="charge_travail_bien" name="charge_travail" value="Bien"> Bien
@@ -99,7 +115,9 @@
                     <input type="radio" id="charge_travail_plutot_mal" name="charge_travail" value="Plutôt mal"> Plutôt mal
                     <input type="radio" id="charge_travail_mal" name="charge_travail" value="Mal"> Mal
                 </div>
+                </form>
 
+                <form action="" method="post">
                 <div class="form-group">
                     <label for="charge_travail">10. Avez-vous des idées ou des suggestions pour améliorer vos conditions de travail et réduire le stress ?</label><br>
                     <input type="radio" id="charge_travail_bien" name="charge_travail" value="Bien"> Bien
@@ -108,7 +126,9 @@
                     <input type="radio" id="charge_travail_plutot_mal" name="charge_travail" value="Plutôt mal"> Plutôt mal
                     <input type="radio" id="charge_travail_mal" name="charge_travail" value="Mal"> Mal
                 </div>
+                </form>
 
+                <form action="" method="post">
                 <div class="form-group">
                     <label for="charge_travail">11. Comment vous sentez-vous le matin à l'idée de devoir affronter une autre journée de travail ?</label><br>
                     <input type="radio" id="charge_travail_bien" name="charge_travail" value="Bien"> Bien
@@ -117,7 +137,9 @@
                     <input type="radio" id="charge_travail_plutot_mal" name="charge_travail" value="Plutôt mal"> Plutôt mal
                     <input type="radio" id="charge_travail_mal" name="charge_travail" value="Mal"> Mal
                 </div>
+                </form>
 
+                <form action="" method="post">
                 <div class="form-group">
                     <label for="charge_travail">12. Comment ressentez vous votre relation avec vos collègues ?</label><br>
                     <input type="radio" id="charge_travail_bien" name="charge_travail" value="Bien"> Bien
@@ -126,7 +148,9 @@
                     <input type="radio" id="charge_travail_plutot_mal" name="charge_travail" value="Plutôt mal"> Plutôt mal
                     <input type="radio" id="charge_travail_mal" name="charge_travail" value="Mal"> Mal
                 </div>
+                </form>
 
+                <form action="" method="post">
                 <div class="form-group">
                     <label for="charge_travail">13. Est ce que vous pensez que votre travail est gratifiant ?</label><br>
                     <input type="radio" id="charge_travail_bien" name="charge_travail" value="Bien"> Bien
@@ -135,7 +159,9 @@
                     <input type="radio" id="charge_travail_plutot_mal" name="charge_travail" value="Plutôt mal"> Plutôt mal
                     <input type="radio" id="charge_travail_mal" name="charge_travail" value="Mal"> Mal
                 </div>
+                </form>
 
+                <form action="" method="post">
                 <div class="form-group">
                     <label for="charge_travail">14. Pensez vous avoir une atmosphère détendue sur votre lieu de travail ?</label><br>
                     <input type="radio" id="charge_travail_bien" name="charge_travail" value="Bien"> Bien
@@ -144,7 +170,9 @@
                     <input type="radio" id="charge_travail_plutot_mal" name="charge_travail" value="Plutôt mal"> Plutôt mal
                     <input type="radio" id="charge_travail_mal" name="charge_travail" value="Mal"> Mal
                 </div>
+                </form>
 
+                <form action="" method="post">
                 <div class="form-group">
                     <label for="charge_travail">15. Êtes vous toujours calme ou détendue sur votre lieu de travail ?</label><br>
                     <input type="radio" id="charge_travail_bien" name="charge_travail" value="Bien"> Bien
@@ -153,7 +181,9 @@
                     <input type="radio" id="charge_travail_plutot_mal" name="charge_travail" value="Plutôt mal"> Plutôt mal
                     <input type="radio" id="charge_travail_mal" name="charge_travail" value="Mal"> Mal
                 </div>
+                </form>
 
+                <form action="" method="post">
                 <div class="form-group">
                     <label for="charge_travail">16. Souhaitez-vous rester dans votre travail actuel ?</label><br>
                     <input type="radio" id="charge_travail_bien" name="charge_travail" value="Bien"> Bien
@@ -162,9 +192,9 @@
                     <input type="radio" id="charge_travail_plutot_mal" name="charge_travail" value="Plutôt mal"> Plutôt mal
                     <input type="radio" id="charge_travail_mal" name="charge_travail" value="Mal"> Mal
                 </div>
+                </form>
                 
                 <button class="button_send"type="submit">Envoyer</button>
-            </form>
         </div>
     </div>
 </body>
